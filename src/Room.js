d4b5d7e229482fe505eece9d4c527a1358b6e812
fizreh <phaeza31@gmail.com>
2020-06-10 20:42:19 +0200

    add extra buttons and temp

--- src/Room.js
+++ src/Room.js
@@ -4,9 +4,8 @@ import './Room.css'
 function Room() {
 let [isLit,setLit] = useState(false);
 let [age,setAge] = useState(23);
+let [temp,setTemp] = useState(72);
 //let hello = `Hello $(isLit) World`;
-//const state = useState(true);
-//console.log("State= ",state);
 
 
 
@@ -15,16 +14,24 @@ return (
 <div className= {`room  ${isLit? "lit": "dark"}`}>
     This Room is  {isLit? "Lit":"Dark"}
 <br/>
-Age: {age}
+Temp: {temp} <button onClick={()=>setTemp(++temp)}> ++ </button> <button onClick={()=>setTemp(--temp)}> -- </button>
 <br/>
-<button onClick={()=>setLit(!isLit)}>Toggle Light
-    </button>
-<br/>
-<button onClick={()=>{
+Age: {age} <button onClick={()=>{
 console.log("increaseAge arrow function");
 setAge(++age);}}>
     Increase Age
     </button>
+<br/>
+
+<button onClick={()=>setLit(!isLit)}>Toggle Light</button>
+<br/>
+<button onClick={()=>{
+console.log("ON Button");
+setLit(true);}}>ON</button>
+<button onClick={()=>{
+console.log("OFF Button");
+setLit(false);}}>OF</button>
+<br/>
 </div>
 );
 }
