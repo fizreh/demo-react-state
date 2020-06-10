0b64f9808c69bd5758cdc91d2400892c35d5885f
fizreh <phaeza31@gmail.com>
2020-06-10 19:33:59 +0200

    update css to toggle

--- src/Room.js
+++ src/Room.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
-
+import './Room.css'
 
 function Room() {
 let [isLit,setLit] = useState(false);
 let [age,setAge] = useState(23);
-
+//let hello = `Hello $(isLit) World`;
 //const state = useState(true);
 //console.log("State= ",state);
 
 
 
 return (
-<div>This Room is  {isLit? "Lit":"Dark"}
+//<div className= {"room "+(isLit?"lit":"dark")}>This Room is  {isLit? "Lit":"Dark"}
+<div className= {`room  ${isLit? "lit": "dark"}`}>
+    This Room is  {isLit? "Lit":"Dark"}
 <br/>
 Age: {age}
 <br/>
