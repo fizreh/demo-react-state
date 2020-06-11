e85aec9fe8587b6c0e15f386d7caab3f1eb2e8cd
fizreh <phaeza31@gmail.com>
2020-06-11 10:45:44 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Room from './Room';
+import './App.css'
 
 function App() {
   
- // const [isLit,setLit] = useState(true);
-  const state = useState(true);
-  console.log(state);
+ 
   return (
     <div><Room> </Room></div>
   );
